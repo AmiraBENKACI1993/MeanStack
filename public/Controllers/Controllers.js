32d67aa570363a3e359b4e69e1b859b94e356c39
amira benkaci <36472635+AmiraBENKACI1993@users.noreply.github.com>
2018-07-19 17:41:24 +0200

    Add files via upload

---
+++ public/Controllers/Controllers.js
@@ -0,0 +1,53 @@
+var myApp = angular.module('myApp',[]);
+myApp.controller('AppCtrl',['$scope','$http', function($scope,$http){
+    console.log("hello controller");
+
+var refresh = function(){
+    $http.get('/contactlist', $scope.contact).success(function(response){
+        
+        console.log("I got a data that i requested");
+        $scope.contactlist = response;
+        $scope.contact = "";
+   });
+
+};
+
+refresh();
+
+   $scope.addContact = function(){ 
+        console.log($scope.contact); 
+        $http.post('/contactlist', $scope.contact).success(function(response){
+            console.log(response);
+            refresh();
+       });   
+};  
+
+$scope.remove = function(id) {
+    console.log(id);
+    $http.delete('/contactlist/' + id).success(function(response){
+        console.log(response);
+        refresh();
+    });
+};
+
+$scope.edit = function(id){
+    console.log(id);
+    $http.get('/contactlist/' + id).success(function(response){
+        $scope.contact = response;
+        console.log(response);
+        
+});
+};
+
+$scope.update = function(id){
+    console.log($scope.contact._id);
+    $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response){
+        refresh();
+    })
+};  
+
+$scope.deselect = function() {
+    $scope.contact = "";
+}
+    
+}]);
